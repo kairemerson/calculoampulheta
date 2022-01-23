c7f6a9716b926b88f388d6877b9db8f7f048b922
kairemerson <kairemerson@gmail.com>
2022-01-22 21:42:14 -0300

    finalizado projeto ampulheta

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# calculoampulheta
-Desafio de lógica criação de ampulheta com javascript
+# Desafio Cálculo Ampulheta
+Desafio de lógica, criação de ampulheta com javascript
+
+![tela3](https://user-images.githubusercontent.com/59590026/150660112-d2aab332-3a7d-4ee1-bf14-89ce357412f8.jpg)
+
+
---
+++ gerarAmpulheta.js
@@ -0,0 +1,121 @@
+let button = document.getElementById("btn")
+let div = document.getElementById("content")
+let str = ""
+let input = document.getElementById("gerador")
+let valor = 0
+button.addEventListener("click",()=>{
+    valor = Number(input.value)
+    div.innerHTML = ""
+    if(valor < 20){
+        alert("Insira um valor acima de 20")
+        return
+    }else{
+        gerarAmpulheta(valor)
+    }
+})
+
+function girarAmpulheta(){
+    setTimeout(()=>{
+        div.innerHTML = ""
+        gerarAmpulhetaInversa(valor)
+    },2000)
+}
+
+function gerarAmpulheta(n){
+    for (let i = 0; i<n; i++){
+        if(i == 0){
+            str = `${"#".repeat(n-i)}`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i == 1){
+            str = `${"#".repeat(n)}`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i ==2){
+            str = `# ${" ".repeat((i/2)-1) + "#".repeat(n-i-2)} #`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i%2==0 && i < n-3){
+            str = `#${" ".repeat((i/2)) + "#".repeat((n-i-2))+ " ".repeat((i/2))}#`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }
+    }
+    for (let i = n; i >= 0; i--){
+        if(i == 0){
+            str = `${"#".repeat(n)}`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i == 1){
+            str = `#${"#" +" ".repeat(n-4)}##`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i ==2){
+            str = `# ${"#" +" ".repeat(n-4-i)}# #`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i%2==0 && i < n-3){
+            str = `#${" ".repeat((i/2)) + "#" + " ".repeat(n-4-i)+"#"+ " ".repeat((i/2))}#`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }
+    }
+    girarAmpulheta()
+}
+
+function gerarAmpulhetaInversa(n){
+    for (let i = 0; i <= n; i++){
+        if(i == 0){
+            str = `${"#".repeat(n)}`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i == 1){
+            str = `#${"#" +" ".repeat(n-4)}##`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i ==2){
+            str = `# ${"#" +" ".repeat(n-4-i)}# #`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i%2==0 && i < n-3){
+            str = `#${" ".repeat((i/2)) + "#" + " ".repeat(n-4-i)+"#"+ " ".repeat((i/2))}#`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }
+    }
+    for (let i = n; i>=0; i--){
+        if(i == 0){
+            str = `${"#".repeat(n-i)}`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i == 1){
+            str = `${"#".repeat(n)}`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i ==2){
+            str = `# ${" ".repeat((i/2)-1) + "#".repeat(n-i-2)} #`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }else if(i%2==0 && i < n-3){
+            str = `#${" ".repeat((i/2)) + "#".repeat((n-i-2))+ " ".repeat((i/2))}#`
+            let pre = document.createElement("pre")
+            pre.innerText = str
+            div.append(pre)
+        }
+    }
+}
